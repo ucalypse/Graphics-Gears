7fcd165bbc4e76e6c9eae02c42957b17c967b0a1
Donald Davis <donald.m.davis@sherwin.com>
2017-03-10 09:01:26 -0500

    added em-mail

--- GraphicsAndGears/Controllers/GearsController.cs
+++ GraphicsAndGears/Controllers/GearsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,15 +9,13 @@ namespace GraphicsAndGears.Controllers
 {
     public class GearsController : Controller
     {
-        Car car = new Car();
-        Customer customer = new Customer();
+        
         // GET: Gears
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Gears/Details/5
         public ActionResult Details(int id)
         {
             return View();
@@ -26,7 +25,7 @@ namespace GraphicsAndGears.Controllers
         {
             return View();
         }
-        // GET: Gears/Create
+       
         public ActionResult Create()
         {
             return View();
@@ -36,7 +35,6 @@ namespace GraphicsAndGears.Controllers
             return View();
         }
 
-        // POST: Gears/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
@@ -55,14 +53,31 @@ namespace GraphicsAndGears.Controllers
         [HttpPost]
         public ActionResult SendEmail(string nameField, string makeField, string modelField, string yearField, string phoneField, string emailField)
         {
-            //make crazy change
+            var car = new Car();
+            var customer = new Customer();
+
             car.Make = makeField;
             car.Model = modelField;
             car.Year = Int32.Parse(yearField);
             customer.Name = nameField;
             customer.Email = emailField;
             customer.Phone = phoneField;
-            throw new Exception("Method not impletmented");
+            
+            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+
+            message.From = new System.Net.Mail.MailAddress("ddavis70@lycos.com");
+            message.To.Add(new System.Net.Mail.MailAddress("davis199994@yahoo.com"));
+
+            message.IsBodyHtml = true;
+            message.BodyEncoding = Encoding.UTF8;
+            message.Subject = "Test";
+            message.Body = car.Make + " " + car.Model + " " + car.Year + "\n" + customer.Name + " " + customer.Email +
+                           " " + customer.Phone;
+
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+            
+            client.Send(message);
+            throw new Exception("Method not implemented");
         }
         // GET: Gears/Edit/5
         public ActionResult Edit(int id)
